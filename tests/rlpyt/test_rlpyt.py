82651a1fcffb8188ab9208d6a27039a462c5454f
Julius Frost <juliustfrost@gmail.com>
2020-03-27 23:55:49 -0400

    make rlpyt test work on windows

--- tests/rlpyt/test_rlpyt.py
+++ tests/rlpyt/test_rlpyt.py
@@ -6,12 +6,11 @@ psutil    ## pip install --user psutil
 pyprind   ## pip install --user pyprind
 """
 
-from rlpyt.samplers.serial.sampler import SerialSampler
-from rlpyt.envs.atari.atari_env import AtariEnv
-from rlpyt.algos.dqn.dqn import DQN
 from rlpyt.agents.dqn.atari.atari_dqn_agent import AtariDqnAgent
+from rlpyt.algos.dqn.dqn import DQN
+from rlpyt.envs.atari.atari_env import AtariEnv
 from rlpyt.runners.minibatch_rl import MinibatchRl
-from rlpyt.utils.logging.context import logger_context
+from rlpyt.samplers.serial.sampler import SerialSampler
 
 
 def build_and_train(game="pong", run_ID=0, cuda_idx=None):
@@ -36,20 +35,10 @@ def build_and_train(game="pong", run_ID=0, cuda_idx=None):
         log_interval_steps=1e3,
         affinity=dict(cuda_idx=cuda_idx),
     )
-    config = dict(game=game)
-    name = "dqn_" + game
-    log_dir = "example_1"
-    with logger_context(log_dir, run_ID, name, config, snapshot_mode="last"):
-        runner.train()
+    runner.train()
 
 
 def test_rlpyt():
-    # import argparse
-    # parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    # parser.add_argument('--game', help='Atari game', default='pong')
-    # parser.add_argument('--run_ID', help='run identifier (logging)', type=int, default=0)
-    # parser.add_argument('--cuda_idx', help='gpu to use ', type=int, default=None)
-    # args = parser.parse_args()
     build_and_train(
         game='pong',
         run_ID=0,
