ff3c27c4e78975b6dfa909d93f03cca6d2315224
Mahroo <47149572+Mahrooo@users.noreply.github.com>
2020-04-03 00:32:39 -0400

    Add files via upload

---
+++ tests/dreamer/envs/test_wrappers.py
@@ -0,0 +1,23 @@
+import gym
+from dreamer.envs.wrappers import Atari, ActionRepeat, NormalizeActions
+
+env = gym.make('AirRaid-ram-v0')
+env.reset()
+for _ in range(1000):
+    env.render()
+    env.step(env.action_space.sample()) # take a random action
+env.close()
+
+name = 'AirRaid'
+env = ActionRepeat(env, 2)
+env = Atari(
+        name, 4, (64, 64), grayscale=False,
+         life_done=True, sticky_actions=True)
+
+#env = gym.make('Pendulum-v0')
+#env = NormalizeActions(env)
+#env.reset()
+#for _ in range(1000):
+    #env.render()
+    #env.step(env.action_space.sample()) # take a random action
+#env.close()
