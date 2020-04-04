ee841006f08aae01c5c625d1d151071ea43c2a11
Julius Frost <juliustfrost@gmail.com>
2020-04-04 13:40:08 -0400

    fix formatting

--- dreamer/models/dense_model.py
+++ dreamer/models/dense_model.py
@@ -1,11 +1,11 @@
+import numpy as np
 import torch
 import torch.distributions as td
 import torch.nn as nn
-import torch.nn.functional as F
-import numpy as np
+
 
 class DenseModel(nn.Module):
-    def __init__(self, lenfeature , shape, layers, units, dist='normal', act=nn.ELU()):
+    def __init__(self, lenfeature, shape, layers, units, dist='normal', act=nn.ELU()):
         super(DenseModel, self).__init__()
         self._shape = shape
         self._layers = layers
@@ -14,13 +14,11 @@ class DenseModel(nn.Module):
         self._act = act
         # For adjusting pytorch to tensforflow
         self._lenfeature = lenfeature
-        #Defining the structure of the NN
-        self.fcfirst = nn.Linear(self._lenfeature , self._units )
-        self.linears = nn.ModuleList([nn.Linear(self._units, self._units) for i in range(self._layers-1)])
-        self.fclast = nn.Linear( self._units, int(np.prod(self._shape)))
+        # Defining the structure of the NN
+        self.fcfirst = nn.Linear(self._lenfeature, self._units)
+        self.linears = nn.ModuleList([nn.Linear(self._units, self._units) for i in range(self._layers - 1)])
+        self.fclast = nn.Linear(self._units, int(np.prod(self._shape)))
 
-
-    
     def __call__(self, features):
         x = features
         x = self._act(self.fcfirst(x))
@@ -28,9 +26,10 @@ class DenseModel(nn.Module):
             x = self._act(l(x))
         x = self.fclast(x)
         # TODO: Seems to do nothing in pytorch code. Come back if there are errors!
-        x = torch.reshape(x, tuple(np.array(np.concatenate((features.shape[:-1], self._shape), axis=0), dtype=np.int)))     #It makes the size of output = (batch_size,)
+        x = torch.reshape(x, tuple(np.array(np.concatenate((features.shape[:-1], self._shape), axis=0),
+                                            dtype=np.int)))  # It makes the size of output = (batch_size,)
         if self._dist == 'normal':
             return td.independent.Independent(td.Normal(x, 1), len(self._shape))
         if self._dist == 'binary':
             return td.independent.Independent(td.Bernoulli(logits=x), len(self._shape))
-        raise NotImplementedError(self._dist)        
+        raise NotImplementedError(self._dist)
--- tests/dreamer/models/test_dense_model.py
+++ tests/dreamer/models/test_dense_model.py
@@ -1,26 +1,26 @@
 import pytest
 import torch
 import torch.nn as nn
+
 from dreamer.models.dense_model import DenseModel
-import numpy as np
-                                                
+
+
 @pytest.mark.parametrize('dist', ['normal', 'binary'])
-def test_DenseModel(dist):
-    shape = ()
+def test_dense_model(dist):
+    shape = (1,)
     units = 20
     feature_size = 20
     layers = 5
     batch_size = 2
-    features = torch.randn((batch_size,feature_size))
-    
+    features = torch.randn((batch_size, feature_size))
+
     try:
-        dense = DenseModel(feature_size , shape, layers, units, dist, nn.ELU())
+        dense = DenseModel(feature_size, shape, layers, units, dist, nn.ELU())
     except NotImplementedError:
         return
 
-    
     output = dense(features)
     sample = output.sample()
-    assert isinstance(sample , torch.Tensor)
+    assert isinstance(sample, torch.Tensor)
     # print(sample.size())
-    assert sample.size() == (batch_size,)
+    assert sample.size() == (batch_size, *shape)
