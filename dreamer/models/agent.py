3cf47250176f35be9d9712905942c0e3ee98dd5c
Julius Frost <juliustfrost@gmail.com>
2020-04-04 12:28:40 -0400

    add testing for agent model and improve methods

--- dreamer/models/agent.py
+++ dreamer/models/agent.py
@@ -34,17 +34,14 @@ class AgentModel(nn.Module):
         self.observation_decoder = ObservationDecoder(embed_size=obs_embed_size, shape=obs_shape)
         self.observation_encoder = ObservationEncoder()
         feature_size = stochastic_size + deterministic_size
+        self.action_size = action_size
         self.action_dist = action_dist
         self.action_decoder = ActionDecoder(action_size, feature_size, action_hidden_size, action_layers, action_dist)
         self.reward_model = DenseModel(feature_size, reward_shape, reward_layers, reward_hidden)
         self.value_model = DenseModel(feature_size, value_shape, value_layers, value_hidden)
 
-    def forward(self, observation: torch.Tensor, prev_action: torch.Tensor, prev_state: RSSMState = None):
-        obs_embed = self.observation_encoder(observation)
-        if prev_state is None:
-            prev_state = self.representation.initial_state(prev_action.size(0), device=prev_action.device,
-                                                           dtype=prev_action.dtype)
-        _, state = self.representation(obs_embed, prev_action, prev_state)
+    def forward(self, observation: torch.Tensor, prev_action: torch.Tensor = None, prev_state: RSSMState = None):
+        state = self.get_state_representation(observation, prev_action, prev_state)
         action = self.policy(state)
         return action
 
@@ -57,9 +54,35 @@ class AgentModel(nn.Module):
             else:
                 action = action_dist.mode()
         else:
-            action = action_dist.rsample()
+            # cannot propagate gradients with one hot distribution
+            action = action_dist.sample()
         return action
 
-    def get_state(self, observation, prev_action, prev_state):
-        state = None
+    def get_state_representation(self, observation: torch.Tensor, prev_action: torch.Tensor = None,
+                                 prev_state: RSSMState = None):
+        """
+
+        :param observation: size(batch, channels, width, height)
+        :param prev_action: size(batch, action_size)
+        :param prev_state: RSSMState: size(batch, state_size)
+        :return: RSSMState
+        """
+        obs_embed = self.observation_encoder(observation)
+        if prev_action is None:
+            prev_action = torch.zeros(observation.size(0), self.action_size,
+                                      device=observation.device, dtype=observation.dtype)
+        if prev_state is None:
+            prev_state = self.representation.initial_state(prev_action.size(0), device=prev_action.device,
+                                                           dtype=prev_action.dtype)
+        _, state = self.representation(obs_embed, prev_action, prev_state)
+        return state
+
+    def get_state_transition(self, prev_action: torch.Tensor, prev_state: RSSMState):
+        """
+
+        :param prev_action: size(batch, action_size)
+        :param prev_state: RSSMState: size(batch, state_size)
+        :return: RSSMState
+        """
+        state = self.transition(prev_action, prev_state)
         return state
---
+++ tests/dreamer/models/test_agent.py
@@ -0,0 +1,29 @@
+import torch
+import pytest
+from dreamer.models.agent import AgentModel
+
+
+@pytest.mark.parametrize('dist', ['tanh_normal', 'one_hot'])
+def test_agent(dist):
+    batch_size = 1
+    action_size = 2
+    deterministic_size = 200
+    obs_shape = (3, 64, 64)
+    agent_model = AgentModel(action_size, deterministic_size=deterministic_size, obs_shape=obs_shape, action_dist=dist)
+
+    observation = torch.randn(batch_size, *obs_shape)
+    prev_action = None
+    prev_state = None
+
+    state = agent_model.get_state_representation(observation, prev_action, prev_state)
+    assert state.deter.shape == (batch_size, deterministic_size)
+
+    action = agent_model.policy(state)
+    assert action.shape == (batch_size, action_size)
+
+    agent_model.eval()
+    eval_action = agent_model(observation, prev_action, prev_state)
+    assert eval_action.shape == (batch_size, action_size)
+
+    next_state = agent_model.get_state_transition(action, state)
+    assert next_state.deter.shape == (batch_size, deterministic_size)
