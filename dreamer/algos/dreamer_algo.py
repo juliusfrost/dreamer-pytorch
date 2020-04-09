fixed learning rate scaling for model, value, action models

dreamer/algos/dreamer_algo.py
@@ -45,7 +45,10 @@ class Dreamer(RlAlgorithm):
         self.update_counter = 0
         # dummy model parameters
         self.model_params = (torch.zeros(0, requires_grad=True),)
-        self.optimizer = optim.Adam(self.model_params)
+        self.optimizer = optim.Adam(self.model_params, lr=self.model_lr)
+        self.model_weight = self.model_lr / self.model_lr
+        self.value_weight = self.value_lr / self.model_lr
+        self.actor_weight = self.actor_lr / self.model_lr
         self.type = type
 
     def initialize(self, agent, n_itr, batch_spec, mid_batch_reset, examples, world_size=1, rank=0):
@@ -126,7 +129,7 @@ class Dreamer(RlAlgorithm):
         model_loss = self.model_loss(observation, prior, post, reward)
         actor_loss = self.actor_loss(discount, returns)
         value_loss = self.value_loss(imag_feat, discount, returns)
-        return self.model_lr * model_loss + self.actor_lr * actor_loss + self.value_lr * value_loss
+        return self.model_weight * model_loss + self.actor_weight * actor_loss + self.value_weight * value_loss
 
     def model_loss(self, observation: torch.Tensor, prior: RSSMState, post: RSSMState, reward: torch.Tensor):
         """