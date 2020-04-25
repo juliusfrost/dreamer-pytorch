pass grads through for dm_control and fix leaf gradient error

dreamer/models/agent.py
@@ -64,7 +64,7 @@ class AgentModel(nn.Module):
         action_dist = self.action_decoder(feat)
         if self.action_dist == 'tanh_normal':
             if self.training:  # use agent.train(bool) or agent.eval()
-                action = action_dist.sample()
+                action = action_dist.rsample()
             else:
                 action = action_dist.mode()
         elif self.action_dist == 'one_hot':