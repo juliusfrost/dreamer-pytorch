d7b84040d312b4923cca49d971c907c2b30ee2af
AliengirlLiv <oliviagracewatkins@gmail.com>
2020-04-27 08:59:05 -0700

    fix discounting

--- dreamer/algos/dreamer_algo.py
+++ dreamer/algos/dreamer_algo.py
@@ -258,8 +258,7 @@ class Dreamer(RlAlgorithm):
         discount_arr = self.discount * torch.ones_like(imag_reward)
         returns = self.compute_return(imag_reward[:-1], value[:-1], discount_arr[:-1],
                                       bootstrap=value[-1], lambda_=self.discount_lambda)
-        discount = torch.cumprod(discount_arr[:-1], 1)
-
+        discount = torch.cumprod(discount_arr[:-1], 0)
         actor_loss = -torch.mean(discount * returns)
 
         # ------------------------------------------  Gradient Barrier  ------------------------------------------------
