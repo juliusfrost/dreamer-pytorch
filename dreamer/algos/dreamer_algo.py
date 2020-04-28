92e6460533fbd3b83caf6f24051efda0acca70f2
AliengirlLiv <oliviagracewatkins@gmail.com>
2020-04-28 13:26:54 -0700

    use og hparams

--- dreamer/algos/dreamer_algo.py
+++ dreamer/algos/dreamer_algo.py
@@ -54,7 +54,7 @@ class Dreamer(RlAlgorithm):
             free_nats=3,
             kl_scale=1,
             type=torch.float,
-            prefill=1000,
+            prefill=5000,
             log_video=True,
             video_every=int(1e1),
             video_summary_t=25,
