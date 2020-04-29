fix gradients. See https://colab.research.google.com/drive/1IK_aKrwJMpQQq_zLy0MWYXAXCpNdTLA5

dreamer/algos/dreamer_algo.py
@@ -145,21 +145,23 @@ class Dreamer(RlAlgorithm):
             buffed_samples = buffer_to(samples_from_replay, self.agent.device)
             model_loss, actor_loss, value_loss, loss_info = self.loss(buffed_samples, itr, i)
 
-            self.model_optimizer.zero_grad()
             self.actor_optimizer.zero_grad()
-            self.value_optimizer.zero_grad()
-
-            loss = model_loss + value_loss + actor_loss
-            loss.backward()
-
-            torch.nn.utils.clip_grad_norm_(get_parameters(self.model_modules), self.grad_clip)
+            actor_loss.backward()
             torch.nn.utils.clip_grad_norm_(get_parameters(self.actor_modules), self.grad_clip)
-            torch.nn.utils.clip_grad_norm_(get_parameters(self.value_modules), self.grad_clip)
-
-            self.model_optimizer.step()
             self.actor_optimizer.step()
+
+            self.value_optimizer.zero_grad()
+            value_loss.backward()
+            torch.nn.utils.clip_grad_norm_(get_parameters(self.value_modules), self.grad_clip)
             self.value_optimizer.step()
 
+            self.model_optimizer.zero_grad()
+            model_loss.backward()
+            torch.nn.utils.clip_grad_norm_(get_parameters(self.model_modules), self.grad_clip)
+            self.model_optimizer.step()
+
+            with torch.no_grad():
+                loss = model_loss + actor_loss + value_loss
             opt_info.loss.append(loss.item())
             for field in loss_info_fields:
                 if hasattr(opt_info, field):
@@ -241,7 +243,8 @@ class Dreamer(RlAlgorithm):
                 flat_post = buffer_method(post, 'reshape', batch_size, -1)
         # Rollout the policy for self.horizon steps. Variable names with imag_ indicate this data is imagined not real.
         # imag_feat shape is [horizon, batch_t * batch_b, feature_size]
-        imag_dist, _ = model.rollout.rollout_policy(self.horizon, model.policy, flat_post)
+        with FreezeParameters(self.model_modules):
+            imag_dist, _ = model.rollout.rollout_policy(self.horizon, model.policy, flat_post)
 
         # Use state features (deterministic and stochastic) to predict the image and reward
         imag_feat = get_feat(imag_dist)  # [horizon, batch_t * batch_b, feature_size]