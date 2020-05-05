b32d2fbd6afe7b42062ecf0dddf3f2b2d87d4835
Eric Weiner <eric@mcweiner.org>
2020-05-05 10:00:18 -0400

    Added basic test of random state selection

--- tests/counterfactuals/state_selection.py
+++ tests/counterfactuals/state_selection.py
@@ -1,12 +1,11 @@
-from counterfactuals.dataset import Trajectory
+from counterfactuals.dataset import TrajectoryDataset
 from counterfactuals.state_selection.random_state_selection import RandomStateSelection
 from tests.counterfactuals.dataset import add_data
 
-def test_random_state_selection(dataset):
+def test_random_state_selection():
+    dataset = TrajectoryDataset()
     dataset = add_data(dataset)
     rss = RandomStateSelection(dataset)
-    results = rss.select_indices(5)
-    assert len(results == 5)
-    for result in results:
-        assert isinstance(result, Trajectory)
-
+    results = rss.select_indices(3)
+    assert len(results) == 2
+    assert len(results[0]) == 3
