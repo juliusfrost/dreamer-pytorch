9e911e5a8c202072f6126c408374214a8df246b0
parth <parthjaggi28@gmail.com>
2020-05-17 18:17:32 -0400

    kl clamp fix;

--- dreamer/algos/dreamer_algo.py
+++ dreamer/algos/dreamer_algo.py
@@ -220,7 +220,7 @@ class Dreamer(RlAlgorithm):
         prior_dist = get_dist(prior)
         post_dist = get_dist(post)
         div = torch.mean(torch.distributions.kl.kl_divergence(post_dist, prior_dist))
-        div = torch.clamp(div, -float('inf'), self.free_nats)
+        div = torch.clamp(div, self.free_nats, float('inf'))
         model_loss = self.kl_scale * div + reward_loss + image_loss
 
         # ------------------------------------------  Gradient Barrier  ------------------------------------------------
